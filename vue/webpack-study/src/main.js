2bfe9d9cbf1f92fb12ae772932e33560f83f81bc
zhoushengmin <624109752@qq.com>
2019-02-08 20:12:46 +0800

    vue-study

--- vue/webpack-study/src/main.js
+++ vue/webpack-study/src/main.js
@@ -1,46 +1,20 @@
-// 这是 main.js 是我们项目的JS入口文件
-
-// 1. 导入 Jquery
-// import *** from *** 是ES6中导入模块的方式
-// 由于 ES6的代码，太高级了，浏览器解析不了，所以，这一行执行会报错
 import $ from 'jquery'
-// const $ = require('jquery')
-
-// 使用 import 语法，导入 CSS样式表
+//const $ = require('jquery)
+//导入css样式表
+//1需要安装npm i style-loader css-loader -D
+//2.需要在webpack.config.js中配置节点module,在rules数组中存放了所有第三方文件的匹配和处理规则
 import './css/index.css'
+//npm i less-loader -D, npm i less -D
 import './css/index.less'
-import './css/index.scss'
-// 注意： webpack, 默认只能打包处理 JS 类型的文件，无法处理 其它的非 JS 类型的文件；
-// 如果要处理 非JS类型的文件，我们需要手动安装一些 合适 第三方 loader 加载器；
-// 1. 如果想要打包处理 css 文件，需要安装 cnpm i style-loader css-loader -D
-// 2. 打开 webpack.config.js 这个配置文件，在 里面，新增一个 配置节点，叫做 module, 它是一个对象；在 这个 module 对象身上，有个 rules 属性，这个 rules 属性是个 数组；这个数组中，存放了，所有第三方文件的 匹配和 处理规则；
-
-
-// 注意： webpack 处理第三方文件类型的过程：
-// 1. 发现这个 要处理的文件不是JS文件，然后就去 配置文件中，查找有没有对应的第三方 loader 规则
-// 2. 如果能找到对应的规则， 就会调用 对应的 loader 处理 这种文件类型；
-// 3. 在调用loader 的时候，是从后往前调用的；
-// 4. 当最后的一个 loader 调用完毕，会把 处理的结果，直接交给 webpack 进行 打包合并，最终输出到  bundle.js 中去
-
-
-
-
-$(function () {
-  $('li:odd').css('backgroundColor', 'yellow')
-  $('li:even').css('backgroundColor', function () {
-    return '#' + 'D97634'
+//npm i sass-loader -D, cnpm i sass-loader -D 
+// import './css/index.scss'
+$(function(){
+  $('li:odd').css('backgroundColor','orange')
+  $('li:even').css('backgroundColor',function(){
+    return '#'+'D97634'
   })
 })
-
-
-// 经过刚才的演示，Webpack 可以做什么事情？？？
-// 1. webpack 能够处理 JS 文件的互相依赖关系；
-// 2. webpack 能够处理JS的兼容问题，把 高级的、浏览器不是别的语法，转为 低级的，浏览器能正常识别的语法
-
-// 刚才运行的命令格式：    webpack  要打包的文件的路径  打包好的输出文件的路径
-
-
-
+//npm run dev
 // 使用 webpack-dev-server 这个工具，来实现自动打包编译的功能
 // 1. 运行 npm i webpack-dev-server -D 把这个工具安装到项目的本地开发依赖
 // 2. 安装完毕后，这个 工具的用法， 和 webpack 命令的用法，完全一样
